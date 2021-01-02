55697e571560318d6d1581631bffd36b2ccf3407
lila <lilareliga@wp.pl>
2021-01-02 15:33:18 +0100

    create hall of fame/ ranking view
    1. modify html and scss for the text exclusive for ranking view
    2. add function in app.js to switch views between main view and hall of fame/ranking view

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,24 @@
-export const App = ({options}) => {
-}
+export const App = ({ options }) => {
+  const buttonRanking = document.querySelector('.button--ranking');
 
+  function switchToHallOfFame() {
+    buttonRanking.lastElementChild.innerText = 'Rules';
+    buttonRanking.firstElementChild.classList = 'fas fa-graduation-cap';
 
+    document.querySelector('.mode__hall').hidden = false;
+    document.querySelector('.mode__rules').hidden = true;
+
+    buttonRanking.addEventListener('click', switchToRules);
+    buttonRanking.removeEventListener('click', switchToHallOfFame);
+  }
+  function switchToRules() {
+    buttonRanking.lastElementChild.innerText = 'Hall of fame';
+    buttonRanking.firstElementChild.classList = 'fas fa-id-badge';
+    document.querySelector('.mode__hall').hidden = true;
+    document.querySelector('.mode__rules').hidden = false;
+    buttonRanking.addEventListener('click', switchToHallOfFame);
+    buttonRanking.removeEventListener('click', switchToRules);
+  }
+
+  switchToRules();
+};
