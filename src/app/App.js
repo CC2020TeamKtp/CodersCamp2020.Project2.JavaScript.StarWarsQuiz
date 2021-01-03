bd3c7bc0c22cdeb2a7972250a832adaea8c2913d
lilareliga <74837771+lilareliga@users.noreply.github.com>
2021-01-03 15:23:03 +0100

    create hall of fame/ ranking view (#20)
    * create hall of fame/ ranking view
    
    1. modify html and scss for the text exclusive for ranking view
    2. add function in app.js to switch views between main view and hall of fame/ranking view
    
    * after 1st review
    
    - change grid to table
    - add class to span in rankingBtn in html
    - rewrite js. file according to suggestions
    - change body padding to 1rem
    
    * add media query for html table (width: 480-768px)
    
    Co-authored-by: lila <lilareliga@wp.pl>

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,22 @@
-export const App = ({options}) => {
-}
+export const App = ({ options }) => {
+  const rankingBtn = document.querySelector('.button--ranking');
+  const modeHall = document.querySelector('.mode__hall');
+  const modeRules = document.querySelector('.mode__rules');
+  const rankingBtnTxt = rankingBtn.querySelector('.button__text');
+  const rankingBtnIcon = rankingBtn.querySelector('.fas');
 
+  modeHall.hidden = true;
+  rankingBtn.addEventListener('click', switchBtn);
+  function switchBtn() {
+    if (rankingBtnTxt.innerHTML === 'Hall of fame') {
+      rankingBtnTxt.innerHTML = 'Rules';
+      rankingBtnIcon.classList = 'fas fa-graduation-cap';
+    } else {
+      rankingBtnTxt.innerHTML = 'Hall of fame';
+      rankingBtnIcon.classList = 'fas fa-id-badge';
+    }
 
+    modeHall.hidden = !modeHall.hidden;
+    modeRules.hidden = !modeRules.hidden;
+  }
+};
