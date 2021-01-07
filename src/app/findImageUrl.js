68ba7e6a1dfeb82ea9e26deb58aa5c899866507f
kalush81 <krzysiek.kaluza@gmail.com>
2021-01-07 02:15:56 +0100

    Refactor findImageUrl function

--- src/app/findImageUrl.js
+++ src/app/findImageUrl.js
@@ -1,7 +1,3 @@
 export function findImageUrl(mode, idx) {
-    console.log('index', idx)
-    function emptySpacesToUnderscore(name) {
-        return `/static/img/modes/${mode}/${idx}.jpg`
-    }
-    return emptySpacesToUnderscore(name)
+  return `/static/img/modes/${mode}/${idx}.jpg`;
 }
