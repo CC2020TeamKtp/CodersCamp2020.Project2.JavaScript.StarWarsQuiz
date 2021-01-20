a887ab25ee2d2293906df85abd928ca82478d4bf
lilareliga <lilareliga@gmail.com>
2021-01-20 12:13:45 +0100

    create new component ControlButtons
    - remove styles folder
    - move code related to buttons to the ControlButtons folder
    - remove code related to settings view
    - create callback functions for communication between ControlButtons & app

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -5,16 +5,10 @@ import ApiDataFetcher from '../../services/ApiDataFetcher/ApiDataFetcher';
 import { GameEngine } from '../../services/GameEngine/GameEngine';
 import { Timer } from '../Timer';
 import { GameView } from '../GameView';
+import { ControlButtons } from '../ControlButtons';
 
 export const App = ({ options }) => {
-  const rankingBtn = document.querySelector('.button--ranking');
   const modeRules = document.querySelector('.mode__rules');
-  const rankingBtnTxt = rankingBtn.querySelector('.button__text');
-  const rankingBtnIcon = rankingBtn.querySelector('.fas');
-  const btnSettings = document.querySelector('.button--settings');
-  const formSettings = document.querySelector('form');
-  const btnBack = document.querySelector('.button--back');
-  const playTheGame = document.querySelector('.button--play');
   const inGameMode = document.querySelector('.mode__game-in-progress');
 
   const config = {
@@ -30,96 +24,51 @@ export const App = ({ options }) => {
       hallOfFame.saveResult(result) || hallOfFame.update(),
   });
 
-  //do wykorzystania także, gdy skończą się pytania
-  function handleGameOver() {
-    gameOver.display();
-    timer.hide();
-    inGameMode.hidden = true;
-    rankingBtn.hidden = false;
-    playTheGame.hidden = false;
-    btnSettings.hidden = false;
-    switchToHall();
-    gameMode.enableButtons();
-  }
-
   const gameMode = new GameModeSelect(config);
 
   const timer = new Timer({
     config: config,
     announceGameOver: handleGameOver,
   });
+
+  const controlButtons = new ControlButtons({
+    handleSwitchToRules: () => hallOfFame.hide() || (modeRules.hidden = false),
+    handleSwitchToHall: () => hallOfFame.display() || (modeRules.hidden = true),
+    handlePlayTheGame: () => play(config.selectedGame),
+  });
+
+  controlButtons.display();
+  //do wykorzystania także, gdy skończą się pytania
+  function handleGameOver() {
+    gameOver.display();
+    timer.hide();
+    inGameMode.hidden = true;
+    controlButtons.display();
+    gameMode.enableButtons();
+    hallOfFame.display();
+    modeRules.hidden = true;
+  }
+
   /*to się przydaje do testów w sytuacji, gdy skończyły się pytania
   document
     .querySelector('.answers__option')
     .addEventListener('click', () => handleGameOver());*/
 
-  btnSettings.addEventListener('click', () => {
-    btnSettings.hidden = true;
-
-    document.querySelector('.mode__rules').hidden = true;
-    document.querySelector('.mode__type').hidden = true;
-    document.querySelector('.button--ranking').hidden = true;
-    document.querySelector('.button--play').hidden = true;
-    document.querySelector('.question__image').hidden = true;
-    hallOfFame.hide();
-    formSettings.hidden = false;
-    btnBack.hidden = false;
-  });
-
-  btnBack.addEventListener('click', () => {
-    window.location.reload();
-  });
-
-  function onLoadHide() {
-    formSettings.hidden = true;
-    btnBack.hidden = true;
-  }
-
-  onLoadHide();
-
-  rankingBtn.addEventListener('click', switchBtn);
-
-  function switchToRules() {
-    rankingBtnTxt.innerText = 'Hall of fame';
-    rankingBtnIcon.classList = 'fas fa-id-badge';
-    hallOfFame.hide();
-    modeRules.hidden = false;
-  }
-
-  function switchToHall() {
-    rankingBtnTxt.innerText = 'Rules';
-    rankingBtnIcon.classList = 'fas fa-graduation-cap';
-    hallOfFame.display();
-    modeRules.hidden = true;
-  }
-
-  function switchBtn() {
-    rankingBtnTxt.innerText === 'Hall of fame'
-      ? switchToHall()
-      : switchToRules();
-  }
-
   //get data from API based on active game mode
   const apiDataFetcher = new ApiDataFetcher(options.swApiBaseUrl);
-  playTheGame.addEventListener('click', () => play(config.selectedGame));
 
   async function play(gameMode) {
     const quiz = new GameEngine(gameMode, apiDataFetcher);
-
     await quiz.fetchAllQuestionsForMode(gameMode);
-
     const gameView = new GameView();
     const nextQuestion = quiz.generateNextQuestion();
     gameView.displayQuestion(nextQuestion);
+    setGameInProgressView();
   }
-
-  playTheGame.addEventListener('click', setGameInProgressView);
 
   function setGameInProgressView() {
     modeRules.hidden = true;
-    rankingBtn.hidden = true;
-    playTheGame.hidden = true;
-    btnSettings.hidden = true;
+    controlButtons.hide();
     inGameMode.hidden = false;
     timer.display();
     hallOfFame.hide();
